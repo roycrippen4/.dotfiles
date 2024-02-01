8d17b6b4947764cfb96beed293ffe4ccfc20949b
roy.crippen4 <roy.crippen4@archarithms.com>
2024-02-01 01:42:42 -0500

    (uhhh) Made a really shitty colorscheme

---
+++ nvim/.config/nvim/lua/custom/themes/material_dark.lua
@@ -0,0 +1,59 @@
+-- This is an abortion of a color scheme
+
+local M = {}
+
+M.base_30 = {
+  white = '#EEFFFF',
+  darker_black = '#000000',
+  black = '#20202A', --  nvim bg
+  black2 = '#25252f',
+  one_bg = '#212121',
+  one_bg2 = '#1A1A1A',
+  one_bg3 = '#000000',
+  grey = '#484852',
+  grey_fg = '#515d99',
+  grey_fg2 = '#5e6aaa',
+  light_grey = '#717CB4',
+  red = '#F07178',
+  baby_pink = '#FFA6B4',
+  pink = '#FF9CAC',
+  line = '#323232', -- for lines like vertsplit
+  green = '#C3E88D',
+  vibrant_green = '#ABCF76',
+  blue = '#82AAFF',
+  nord_blue = '#B0C9FF',
+  yellow = '#E6B455',
+  sun = '#FFCB6B',
+  purple = '#C792EA',
+  dark_purple = '#B480D6',
+  teal = '#008080',
+  orange = '#F78C6C',
+  cyan = '#89DDFF',
+  statusline_bg = '#262630',
+  lightbg = '#2e2e38',
+  pmenu_bg = '#ebb9b9',
+  folder_bg = '#b8dceb',
+}
+
+M.base_16 = {
+  base00 = '#000000',
+  base01 = '#1A1A1A',
+  base02 = '#20202A',
+  base03 = '#212134',
+  base04 = '#26374f',
+  base05 = '#38385f',
+  base06 = '#aac0cb',
+  base07 = '#71C6E7',
+  base08 = '#89DDFF',
+  base09 = '#E6B455',
+  base0A = '#DC6068',
+  base0B = '#ABCF76',
+  base0C = '#F78C6C',
+  base0D = '#E2795B',
+  base0E = '#B480D6',
+  base0F = '#F79CAC',
+}
+
+M.type = 'dark'
+
+return M
