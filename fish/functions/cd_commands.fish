17efb50fb558e99bc0e8de3754011774f62a9819
roy.crippen4 <roy.crippen4@archarithms.com>
2024-04-05 20:37:26 -0400

    feat(fish): added fish config

---
+++ fish/functions/cd_commands.fish
@@ -0,0 +1,27 @@
+function go_to_nvim_config 
+  c "$HOME/.dotfiles/nvim/.config/nvim/"
+end
+
+function go_to_home_config
+  c "$HOME/.dotfiles/home"
+end
+
+function go_to_kitty_config 
+  c "$HOME/.dotfiles/kitty/.config/kitty"
+end
+
+function go_to_neodev_config
+	c "$HOME/.dotfiles/nvim/.config/nvim/dev"
+end
+
+function go_to_hypr_config
+	c "$HOME/.dotfiles/hypr/.config/hypr"
+end
+
+function go_to_wofi_config
+	c "$HOME/.dotfiles/wofi/.config/wofi"
+end
+
+function go_to_waybar_config
+	c "$HOME/.dotfiles/waybar/.config/waybar"
+end
--- home/.bin/nvim.sh
+++ home/.bin/nvim.sh
@@ -8,40 +8,40 @@ VIM_CMD=""       # Empty by default
 
 # Function to display help message
 usage() {
-  echo "Usage: $0 [-c] [-d] [-D] [-r] [-v] [-h] [path]"
-  echo "  -c cmd        Launch Neovim and execute a vimscript command"
-  echo "  -d            Run in debug mode"
-  echo "  -D            Run in diff mode"
-  echo "  -r            Enable re-launching of nvim upon exit"
-  echo "  -u            Launch Neovim without loading config"
-  echo "  -v            Display version information and exit"
-  echo "  -h            Display this help and exit"
-  echo "  path          Specify a path to a file or directory (optional)"
+	echo "Usage: $0 [-c] [-d] [-D] [-r] [-v] [-h] [path]"
+	echo "  -c cmd        Launch Neovim and execute a vimscript command"
+	echo "  -d            Run in debug mode"
+	echo "  -D            Run in diff mode"
+	echo "  -r            Enable re-launching of nvim upon exit"
+	echo "  -u            Launch Neovim without loading config"
+	echo "  -v            Display version information and exit"
+	echo "  -h            Display this help and exit"
+	echo "  path          Specify a path to a file or directory (optional)"
 }
 
 # Process options
 while getopts "dDhurvc" opt; do
-  case $opt in
-  D) nvim -d ;;
-  d) DEBUG=1 ;;
-  r) RELAUNCH=1 ;;
-  v)
-    nvim -v
-    exit 0
-    ;;
-  u)
-    nvim -u NONE -U NONE
-    exit 0
-    ;;
-  h)
-    usage
-    exit 0
-    ;;
-  *)
-    usage >&2
-    exit 1
-    ;;
-  esac
+	case $opt in
+	D) nvim -d ;;
+	d) DEBUG=1 ;;
+	r) RELAUNCH=1 ;;
+	v)
+		nvim -v
+		exit 0
+		;;
+	u)
+		nvim -u NONE -U NONE
+		exit 0
+		;;
+	h)
+		usage
+		exit 0
+		;;
+	*)
+		usage >&2
+		exit 1
+		;;
+	esac
 done
 
 # Shift off all the options that were processed
@@ -49,29 +49,29 @@ shift $((OPTIND - 1))
 
 # If there's an additional argument, treat it as the directory or file path
 if [ "$1" ]; then
-  DIRECTORY=$1
+	DIRECTORY=$1
 fi
 
 export DEBUG
 
 nvim_cmd() {
-  if [ -n "$VIM_CMD" ]; then
-    nvim "$DIRECTORY" -c "$VIM_CMD"
-  else
-    nvim "$DIRECTORY"
-  fi
+	if [ -n "$VIM_CMD" ]; then
+		nvim "$DIRECTORY" -c "$VIM_CMD"
+	else
+		nvim "$DIRECTORY"
+	fi
 }
 
 if [ $RELAUNCH -eq 1 ]; then
-  while true; do
-    nvim_cmd
+	while true; do
+		nvim_cmd
 
-    if [ $? -ne 0 ]; then
-      break
-    fi
+		if [ $? -ne 0 ]; then
+			break
+		fi
 
-    echo 'restarting'
-  done
+		echo 'restarting'
+	done
 else
-  nvim_cmd
+	nvim_cmd
 fi
