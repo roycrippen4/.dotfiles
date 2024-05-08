3870d68a9da1ecb29ade6b640b07949da4cbd326
roy.crippen4 <roy.crippen4@archarithms.com>
2024-05-08 01:45:11 -0400

    sdf

--- nvim/.config/nvim/lua/plugins/autotag.lua
+++ nvim/.config/nvim/lua/plugins/autotag.lua
@@ -1,7 +1,7 @@
 return {
   -- 'windwp/nvim-ts-autotag', -- https://github.com/windwp/nvim-ts-autotag -- waiting for PR
   'roycrippen4/nvim-ts-autotag', -- https://github.com/windwp/nvim-ts-autotag
-  branch = 'fix_164',
+  branch = 'main',
   dependencies = 'nvim-treesitter/nvim-treesitter', -- https://github.com/nvim-treesitter/nvim-treesitter
   ft = { 'typescript', 'javascript', 'typescriptreact', 'javascriptreact', 'html', 'svelte', 'jsx', 'tsx', 'markdown', 'mdx' },
   config = function()
