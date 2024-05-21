351bb9d8fae54e41c6c840f5f4871e9cd0940c64
roy.crippen4 <roy.crippen4@archarithms.com>
2024-05-21 10:33:57 -0400

    chore(nvim): remove my fork from plugins

--- nvim/.config/nvim/lua/plugins/tailwind.lua
+++ nvim/.config/nvim/lua/plugins/tailwind.lua
@@ -1,6 +1,6 @@
 return {
   'luckasRanarison/tailwind-tools.nvim', -- https://github.com/luckasRanarison/tailwind-tools.nvim
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
-  ft = { 'typescript', 'javascript', 'typescriptreact', 'javascriptreact', 'svelte' },
+  ft = { 'javascript', 'typescriptreact', 'javascriptreact', 'svelte' },
   opts = { conceal = { enabled = true, min_length = 40 } },
 }
