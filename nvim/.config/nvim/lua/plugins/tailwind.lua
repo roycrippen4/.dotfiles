ce14b0d989856d07520f3f99f486be0a0034be50
roy.crippen4 <roy.crippen4@archarithms.com>
2024-05-22 10:47:57 -0400

    rm tailwind-tools nvim

--- nvim/.config/nvim/lua/plugins/tailwind.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'luckasRanarison/tailwind-tools.nvim', -- https://github.com/luckasRanarison/tailwind-tools.nvim
-  dependencies = { 'nvim-treesitter/nvim-treesitter' },
-  ft = { 'javascript', 'typescriptreact', 'javascriptreact', 'svelte' },
-  opts = { conceal = { enabled = true, min_length = 40 } },
-}
