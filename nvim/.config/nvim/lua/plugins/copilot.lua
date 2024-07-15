7436b24f7ca19ba8a4f491f031cf8928b7d24d32
roy.crippen4 <roy.crippen4@archarithms.com>
2024-07-15 00:00:40 -0400

    nvim: simplify copilot config

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -3,20 +3,16 @@ return {
   event = 'BufReadPost',
   opts = {
     panel = { enabled = false },
-    suggestion = { enabled = true, auto_trigger = true, keymap = { accept = '<M-CR>' } },
+    suggestion = { enabled = true, auto_trigger = true, keymap = { accept = '<M-cr>' } },
     copilot_node_command = vim.fn.expand('$HOME') .. '/.nvm/versions/node/v21.6.2/bin/node',
   },
-  config = function(_, opts)
-    local copilot = require('copilot')
-    local autopairs = require('nvim-autopairs')
-    local suggestion = require('copilot.suggestion')
-
-    copilot.setup(opts)
-
-    vim.keymap.set('i', '<M-CR>', function()
-      autopairs.disable()
-      suggestion.accept()
-      autopairs.enable()
-    end)
-  end,
+  keys = {
+    {
+      mode = 'i',
+      '<M-cr>',
+      function()
+        require('copilot.suggestion').accept()
+      end,
+    },
+  },
 }
