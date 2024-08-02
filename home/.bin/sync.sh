4e62aec73cbda689adea0bef241456d8a2519b33
roy.crippen4 <roy.crippen4@archarithms.com>
2024-08-01 20:39:08 -0400

    fix(sync.sh): fixed text colors not working properly

--- home/.bin/sync.sh
+++ home/.bin/sync.sh
@@ -4,38 +4,38 @@
 
 # GREEN='\033[1;32m'
 BLUE='\033[1;34m'
-RED='\033[1;30m'
+RED='\033[1;31m'
 NC='\033[0m'
 
 # Navigate to the directory of this script (generally ~/.dotfiles/.bin)
 cd "$(dirname "$(readlink -f "$0")")" || exit
 cd ..
 
-echo "${BLUE}Stashing existing changes...${NC}"
+echo -e "${BLUE}Stashing existing changes...${NC}"
 stash_result=$(git stash push -m "sync-dotfiles: Before syncing dotfiles")
 needs_pop=1
 if [ "$stash_result" = "No local changes to save" ]; then
-	needs_pop=0
+  needs_pop=0
 fi
 
-echo "${BLUE}Pulling updates from dotfiles repo...${NC}"
+echo -e "${BLUE}Pulling updates from dotfiles repo...${NC}"
 echo
 git pull origin master
 echo
 
 if [ $needs_pop -eq 1 ]; then
-	echo "${BLUE}Popping stashed changes...${NC}"
-	echo
-	git stash pop
+  echo "${BLUE}Popping stashed changes...${NC}"
+  echo
+  git stash pop
 fi
 
 unmerged_files=$(git diff --name-only --diff-filter=U)
 if [ -n "$unmerged_files" ]; then
-	echo "${RED}The following files have merge conflicts after popping the stash:${NC}"
-	echo
-	printf %"s\n" "$unmerged_files" # Ensure newlines are printed
+  echo -e "${RED}The following files have merge conflicts after popping the stash:${NC}"
+  echo
+  printf %"s\n" "$unmerged_files" # Ensure newlines are printed
 else
-	cd
-	cd "$HOME/.dotfiles/"
-	stow */
+  cd
+  cd "$HOME/.dotfiles/"
+  stow */
 fi
