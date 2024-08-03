1ac1344142e0ef3c029de4e08751899e287855fe
roy.crippen4 <roy.crippen4@archarithms.com>
2024-08-02 23:56:41 -0400

    fix(better-escape): pin to v1.0.0

--- nvim/.config/nvim/lua/plugins/better-escape.lua
+++ nvim/.config/nvim/lua/plugins/better-escape.lua
@@ -1,5 +1,6 @@
-return {
+return { ---@type LazySpec
   'max397574/better-escape.nvim', -- https://github.com/max397574/better-escape.nvim
   event = 'InsertEnter',
+  tag = 'v1.0.0',
   opts = {},
 }
