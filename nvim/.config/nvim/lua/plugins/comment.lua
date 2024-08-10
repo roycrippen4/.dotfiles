22b7639dcf18bd0e8906888cc8a6457cc848b08a
roy.crippen4 <roy.crippen4@archarithms.com>
2024-08-10 19:19:49 -0400

    nvim: use personal fork of Comment.nvim

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,7 +1,6 @@
----@type LazyPluginSpec
-return {
-  'numToStr/Comment.nvim', -- https://github.com/numToStr/Comment.nvim
-  event = 'VimEnter',
+return { ---@type LazyPluginSpec
+  'roycrippen4/Comment.nvim', -- https://github.com/roycrippen4/Comment.nvim
+  event = 'BufEnter',
   dependencies = {
     {
       'JoosepAlviste/nvim-ts-context-commentstring', -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring
@@ -9,7 +8,6 @@ return {
     },
   },
   config = function()
-    ---@diagnostic disable-next-line
     require('Comment').setup({
       pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
       ignore = '^$',
