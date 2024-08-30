6a248bc5a49ba57b325243a58ed130a92f948957
roy.crippen4 <roy.crippen4@archarithms.com>
2024-08-30 16:58:26 -0400

    fix(hyprland): fixed hyprpaper issue

--- waybar/.config/waybar/scripts/wallpaper.sh
+++ waybar/.config/waybar/scripts/wallpaper.sh
@@ -5,10 +5,10 @@ monitor_top=DP-1
 monitor_bot=DP-2
 
 if [ -d "$directory" ]; then
-	random_background=$(ls $directory/*.{jpg,png} | shuf -n 1)
+  random_background=$(ls $directory/*.{jpg,png} | shuf -n 1)
 
-	hyprctl hyprpaper unload all
-	hyprctl hyprpaper preload $random_background
-	hyprctl hyprpaper wallpaper "$monitor_bot, $random_background"
-	hyprctl hyprpaper wallpaper "$monitor_top, $random_background"
+  hyprctl hyprpaper unload all
+  hyprctl hyprpaper preload $random_background
+  hyprctl hyprpaper wallpaper "$monitor_bot, $random_background"
+  hyprctl hyprpaper wallpaper "$monitor_top, $random_background"
 fi
