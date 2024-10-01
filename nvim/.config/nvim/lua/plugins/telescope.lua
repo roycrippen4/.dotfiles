654283633162cdce4af487eaecbb911ae448b1d5
roy.crippen4 <roy.crippen4@archarithms.com>
2024-10-01 01:25:44 -0400

    perf(omg): telescope folds nuke performance holy

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,117 @@
+-- local Path = require('plenary.path')
+-- local conf = require('telescope.config').values
+-- local from_entry = require('telescope.from_entry')
+-- local image_api = require('image')
+-- local image = nil
+-- local is_image_preview = false
+-- local last_file_path = ''
+-- local supported_images = { 'svg', 'png', 'jpg', 'jpeg', 'gif', 'webp', 'avif' }
+
+-- local is_supported_image = function(filepath)
+--   local split_path = vim.split(filepath:lower(), '.', { plain = true })
+--   local extension = split_path[#split_path]
+--   return vim.tbl_contains(supported_images, extension)
+-- end
+
+-- local delete_image = function()
+--   if not image then
+--     return
+--   end
+
+--   image:clear()
+
+--   is_image_preview = false
+-- end
+
+-- local create_image = function(filepath, winid, bufnr)
+--   image = image_api.hijack_buffer(filepath, winid, bufnr)
+
+--   if not image then
+--     return
+--   end
+
+--   vim.schedule(function()
+--     image:render()
+--   end)
+
+--   is_image_preview = true
+-- end
+
+-- local function defaulter(f, default_opts)
+--   default_opts = default_opts or {}
+--   return {
+--     new = function(opts)
+--       if conf.preview == false and not opts.preview then
+--         return false
+--       end
+--       opts.preview = type(opts.preview) ~= 'table' and {} or opts.preview
+--       if type(conf.preview) == 'table' then
+--         for k, v in pairs(conf.preview) do
+--           opts.preview[k] = vim.F.if_nil(opts.preview[k], v)
+--         end
+--       end
+--       return f(opts)
+--     end,
+--     __call = function()
+--       local ok, err = pcall(f(default_opts))
+--       if not ok then
+--         error(debug.traceback(err))
+--       end
+--     end,
+--   }
+-- end
+
+-- local file_previewer = defaulter(function(opts)
+--   opts = opts or {}
+--   local cwd = opts.cwd or vim.uv.cwd()
+--   return previewers.new_buffer_previewer({
+--     title = 'File Preview',
+--     dyn_title = function(_, entry)
+--       return Path:new(from_entry.path(entry, true)):normalize(cwd)
+--     end,
+
+--     get_buffer_by_name = function(_, entry)
+--       return from_entry.path(entry, true)
+--     end,
+
+--     define_preview = function(self, entry, _)
+--       local p = from_entry.path(entry, true)
+--       if p == nil or p == '' then
+--         return
+--       end
+
+--       conf.buffer_previewer_maker(p, self.state.bufnr, {
+--         bufname = self.state.bufname,
+--         winid = self.state.winid,
+--         preview = opts.preview,
+--       })
+--     end,
+
+--     teardown = function(_)
+--       if is_image_preview then
+--         delete_image()
+--       end
+--     end,
+--   })
+-- end, {}).new
+
+-- ---@param filepath string
+-- ---@param bufnr integer
+-- ---@param opts table
+-- local buffer_previewer_maker = function(filepath, bufnr, opts)
+--   if is_image_preview and last_file_path ~= filepath then
+--     delete_image()
+--   end
+
+--   last_file_path = filepath
+
+--   if is_supported_image(filepath) then
+--     create_image(filepath, opts.winid, bufnr)
+--   else
+--     previewers.buffer_previewer_maker(filepath, bufnr, opts)
+--   end
+-- end
+
 local function lsp_definitions()
   local params = vim.lsp.util.make_position_params()
   vim.lsp.buf_request(0, 'textDocument/definition', params, function(_, result, ctx)
@@ -64,124 +178,9 @@ return {
     'folke/which-key.nvim',
   },
   config = function()
-    local Path = require('plenary.path')
     local builtin = require('telescope.builtin')
-    local conf = require('telescope.config').values
-    local from_entry = require('telescope.from_entry')
-    local image_api = require('image')
     local previewers = require('telescope.previewers')
     local telescope = require('telescope')
-
-    local image = nil
-    local is_image_preview = false
-    local last_file_path = ''
-    local supported_images = { 'svg', 'png', 'jpg', 'jpeg', 'gif', 'webp', 'avif' }
-
-    local is_supported_image = function(filepath)
-      local split_path = vim.split(filepath:lower(), '.', { plain = true })
-      local extension = split_path[#split_path]
-      return vim.tbl_contains(supported_images, extension)
-    end
-
-    local delete_image = function()
-      if not image then
-        return
-      end
-
-      image:clear()
-
-      is_image_preview = false
-    end
-
-    local create_image = function(filepath, winid, bufnr)
-      image = image_api.hijack_buffer(filepath, winid, bufnr)
-
-      if not image then
-        return
-      end
-
-      vim.schedule(function()
-        image:render()
-      end)
-
-      is_image_preview = true
-    end
-
-    local function defaulter(f, default_opts)
-      default_opts = default_opts or {}
-      return {
-        new = function(opts)
-          if conf.preview == false and not opts.preview then
-            return false
-          end
-          opts.preview = type(opts.preview) ~= 'table' and {} or opts.preview
-          if type(conf.preview) == 'table' then
-            for k, v in pairs(conf.preview) do
-              opts.preview[k] = vim.F.if_nil(opts.preview[k], v)
-            end
-          end
-          return f(opts)
-        end,
-        __call = function()
-          local ok, err = pcall(f(default_opts))
-          if not ok then
-            error(debug.traceback(err))
-          end
-        end,
-      }
-    end
-
-    local file_previewer = defaulter(function(opts)
-      opts = opts or {}
-      local cwd = opts.cwd or vim.uv.cwd()
-      return previewers.new_buffer_previewer({
-        title = 'File Preview',
-        dyn_title = function(_, entry)
-          return Path:new(from_entry.path(entry, true)):normalize(cwd)
-        end,
-
-        get_buffer_by_name = function(_, entry)
-          return from_entry.path(entry, true)
-        end,
-
-        define_preview = function(self, entry, _)
-          local p = from_entry.path(entry, true)
-          if p == nil or p == '' then
-            return
-          end
-
-          conf.buffer_previewer_maker(p, self.state.bufnr, {
-            bufname = self.state.bufname,
-            winid = self.state.winid,
-            preview = opts.preview,
-          })
-        end,
-
-        teardown = function(_)
-          if is_image_preview then
-            delete_image()
-          end
-        end,
-      })
-    end, {}).new
-
-    ---@param filepath string
-    ---@param bufnr integer
-    ---@param opts table
-    local buffer_previewer_maker = function(filepath, bufnr, opts)
-      if is_image_preview and last_file_path ~= filepath then
-        delete_image()
-      end
-
-      last_file_path = filepath
-
-      if is_supported_image(filepath) then
-        create_image(filepath, opts.winid, bufnr)
-      else
-        previewers.buffer_previewer_maker(filepath, bufnr, opts)
-      end
-    end
-
     require('which-key').add({
       {
         mode = 'n',
@@ -240,8 +239,8 @@ return {
         borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
         color_devicons = true,
         set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
-        file_previewer = file_previewer,
-        buffer_previewer_maker = buffer_previewer_maker,
+        -- file_previewer = file_previewer,
+        -- buffer_previewer_maker = buffer_previewer_maker,
         grep_previewer = previewers.vim_buffer_vimgrep.new,
         qflist_previewer = previewers.vim_buffer_qflist.new,
         mappings = { n = { ['q'] = require('telescope.actions').close } },
