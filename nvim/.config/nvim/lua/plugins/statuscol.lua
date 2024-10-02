ac6b92d11f0d400bd776e0e6696bdeb9e38279d3
roy.crippen4 <roy.crippen4@archarithms.com>
2024-10-02 02:19:12 -0400

    fix(nvim): rm all fold stuff (way too slow)

--- nvim/.config/nvim/lua/plugins/statuscol.lua
+++ nvim/.config/nvim/lua/plugins/statuscol.lua
@@ -1,23 +1,6 @@
-local function provider_selector()
-  return { 'treesitter', 'indent' }
-end
-local function open_all_folds()
-  require('ufo').openAllFolds()
-end
-local function close_all_folds()
-  require('ufo').closeAllFolds()
-end
-
 ---@type LazyPluginSpec
 return {
   'luukvbaal/statuscol.nvim', -- https://github.com/luukvbaal/statuscol.nvim
-  branch = '0.10',
-  dependencies = {
-    'kevinhwang91/nvim-ufo', -- https://github.com/kevinhwang91/nvim-ufo
-    dependencies = 'kevinhwang91/promise-async', -- https://github.com/kevinhwang91/promise-async
-    opts = { provider_selector = provider_selector },
-    keys = { { 'zR', open_all_folds }, { 'zM', close_all_folds } },
-  },
   config = function()
     require('statuscol').setup({
       bt_ignore = { 'nofile', 'prompt', 'terminal' },
@@ -48,8 +31,6 @@ return {
         { sign = { name = { 'todo' }, maxwidth = 1, auto = true } },
         { text = { require('statuscol.builtin').lnumfunc, auto = false } },
         { text = { ' ' } },
-        -- { text = { require('statuscol.builtin').foldfunc }, click = 'v:lua.ScFa' },
-        -- { text = { ' ' } },
         { sign = { namespace = { 'gitsigns' }, maxwidth = 1, auto = false, wrap = false } },
       },
     })
