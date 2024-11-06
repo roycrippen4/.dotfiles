dfc9b8f433e06a8422066ee4fa7044966febe8c3
roy.crippen4 <roy.crippen4@archarithms.com>
2024-11-05 22:52:52 -0500

    fucking undotree

--- nvim/.config/nvim/lua/plugins/undo-tree.lua
+++ nvim/.config/nvim/lua/plugins/undo-tree.lua
@@ -1,30 +1,45 @@
--- local ut_open = false
--- local function ut_toggle()
---   if ut_open then
---     require('nvim-tree.api').tree.toggle()
---     vim.cmd.UndotreeToggle()
---     ut_open = true
---   else
---     vim.cmd.UndotreeToggle()
---     require('nvim-tree.api').tree.toggle()
---     vim.cmd('wincmd l')
---     ut_open = false
---   end
--- end
+local ut_open = false
+local function ut_toggle()
+  if ut_open then
+    require('nvim-tree.api').tree.toggle()
+    vim.cmd.UndotreeToggle()
+    ut_open = true
+  else
+    vim.cmd.UndotreeToggle()
+    require('nvim-tree.api').tree.toggle()
+    vim.cmd('wincmd l')
+    ut_open = false
+  end
+end
 
--- ---@type LazyPluginSpec
--- return {
---   'mbbill/undotree', -- https://github.com/mbbill/undotree
---   keys = {
---     { '<leader>ut', ut_toggle, desc = 'UndoTree' },
---   },
--- }
-
+---@type LazyPluginSpec[]
 return {
-  'jiaoshijie/undotree',
-  dependencies = 'nvim-lua/plenary.nvim',
-  config = true,
-  keys = {
-    { '<leader>u', "<cmd>lua require('undotree').toggle()<cr>" },
+  -- {
+  --   'roycrippen4/undo-tree',
+  --   dependencies = 'nvim-lua/plenary.nvim',
+  --   dev = true,
+  --   opts = {},
+  --   -- config = true,
+  --   keys = {
+  --     { '<leader>uu', "<cmd>lua require('undo-tree').toggle()<cr>" },
+  --   },
+  -- },
+  {
+    'mbbill/undotree', -- https://github.com/mbbill/undotree
+    cmd = 'UndoTreeToggle',
+    lazy = false,
+    keys = {
+      { '<leader>u', ut_toggle, desc = 'UndoTree' },
+    },
   },
 }
+
+-- return {
+--   'jiaoshijie/undotree',
+--   dependencies = 'nvim-lua/plenary.nvim',
+--   dev = true,
+--   config = true,
+--   keys = {
+--     { '<leader>u', "<cmd>lua require('undotree').toggle()<cr>" },
+--   },
+-- }
