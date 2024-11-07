95955660ffdac19294aef461d24d35017e07daf0
roy.crippen4 <roy.crippen4@archarithms.com>
2024-11-06 20:48:14 -0500

    chore(nvim): rm better-escape

--- nvim/.config/nvim/lua/plugins/better-escape.lua
+++
@@ -1,7 +0,0 @@
----@type LazyPluginSpec
-return {
-  -- 'max397574/better-escape.nvim', -- https://github.com/max397574/better-escape.nvim
-  -- event = 'InsertEnter',
-  -- tag = 'v1.0.0',
-  -- opts = {},
-}
