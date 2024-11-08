3c291028b0331f0ddde912e30edde5783a4f4992
roy.crippen4 <roy.crippen4@archarithms.com>
2024-11-07 20:16:05 -0500

    refactor(nvim): move mason-tool-installer into mason config

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,39 +1,77 @@
----@type LazyPluginSpec
+---@type LazyPluginSpec[]
 return {
-  'williamboman/mason.nvim', -- https://github.com/williamboman/mason.nvim
-  keys = {
-    { '<leader>M', '<cmd> Mason <cr>' },
-  },
-  cmd = 'Mason',
-  opts = {
-    ensure_installed = {
-      'lua-language-server',
-      'stylua',
-      'css-lsp',
-      'html-lsp',
-      'json-language-server',
-      'prettier',
-      'prettierd',
-      'rust-analyzer',
-      'typescript-language-server',
-      'clangd',
-      'clang-format',
+  {
+    'williamboman/mason.nvim', -- https://github.com/williamboman/mason.nvim
+    keys = {
+      { '<leader>M', '<cmd> Mason <cr>' },
     },
-    PATH = 'skip',
-    ui = {
-      icons = { package_pending = ' ', package_installed = '󰄳 ', package_uninstalled = ' 󰚌' },
-      keymaps = {
-        toggle_server_expand = '<CR>',
-        install_server = 'i',
-        update_server = 'u',
-        check_server_version = 'c',
-        update_all_servers = 'U',
-        check_outdated_servers = 'C',
-        uninstall_server = 'X',
-        cancel_installation = '<C-c>',
-        toggle_help = '?',
+    cmd = 'Mason',
+    opts = {
+      ensure_installed = {
+        'lua-language-server',
+        'stylua',
+        'css-lsp',
+        'html-lsp',
+        'json-language-server',
+        'prettier',
+        'prettierd',
+        'rust-analyzer',
+        'typescript-language-server',
+        'clangd',
+        'clang-format',
+      },
+      PATH = 'skip',
+      ui = {
+        icons = { package_pending = ' ', package_installed = '󰄳 ', package_uninstalled = ' 󰚌' },
+        keymaps = {
+          toggle_server_expand = '<CR>',
+          install_server = 'i',
+          update_server = 'u',
+          check_server_version = 'c',
+          update_all_servers = 'U',
+          check_outdated_servers = 'C',
+          uninstall_server = 'X',
+          cancel_installation = '<C-c>',
+          toggle_help = '?',
+        },
+      },
+      max_concurrent_installers = 10,
+    },
+  },
+  {
+    'WhoIsSethDaniel/mason-tool-installer.nvim', -- https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim
+    lazy = false,
+    opts = {
+      ensure_installed = {
+        'black',
+        'clang-format',
+        'clangd',
+        'codelldb',
+        'css-lsp',
+        'docker-compose-language-service',
+        'dockerfile-language-server',
+        'eslint-lsp',
+        'gopls',
+        'html-lsp',
+        'htmx-lsp',
+        'hyprls',
+        'json-lsp',
+        'lua-language-server',
+        'marksman',
+        'prettier',
+        'prettierd',
+        'pyright',
+        'rust-analyzer',
+        'shfmt',
+        'stylua',
+        'svelte-language-server',
+        'tailwindcss-language-server',
+        'taplo',
+        'typescript-language-server',
+        'yaml-language-server',
+        'yamlfmt',
+        -- 'zls',
       },
     },
-    max_concurrent_installers = 10,
   },
 }
