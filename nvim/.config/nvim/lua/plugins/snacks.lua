0f21b8dbef88f1bd46511984d44143b57eb89228
roy.crippen4 <roy.crippen4@archarithms.com>
2024-11-07 23:42:21 -0500

    nvim: trying out folke's lazygit thing

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,12 +1,34 @@
+---@module "snacks"
+
+---@type LazyPluginSpec
 return {
   'folke/snacks.nvim',
   priority = 1000,
   lazy = false,
-  ---@module "snacks"
   ---@type snacks.Config
   opts = {
     notifier = { timeout = 5000 },
     statuscolumn = { enabled = false },
+    words = { enabled = false },
+    lazygit = {
+      configure = true,
+      theme_path = vim.fs.normalize(vim.fn.stdpath('cache') .. '/lazygit-theme.yml'),
+      theme = {
+        [241] = { fg = 'Special' },
+        activeBorderColor = { fg = 'MatchParen', bold = true },
+        cherryPickedCommitBgColor = { fg = 'Identifier' },
+        cherryPickedCommitFgColor = { fg = 'Function' },
+        defaultFgColor = { fg = 'Normal' },
+        inactiveBorderColor = { fg = 'FloatBorder' },
+        optionsTextColor = { fg = 'Function' },
+        searchingActiveBorderColor = { fg = 'MatchParen', bold = true },
+        selectedLineBgColor = { bg = 'Visual' }, -- set to `default` to have no background colour
+        unstagedChangesColor = { fg = 'DiagnosticError' },
+      },
+      win = {
+        style = 'lazygit',
+      },
+    },
   },
   init = function()
     vim.api.nvim_create_autocmd('VimEnter', {
@@ -19,6 +41,8 @@ return {
         end
         vim.print = _G.log
         print = _G.log
+
+        vim.keymap.set('n', '<F11>', Snacks.lazygit.open)
       end,
     })
   end,
