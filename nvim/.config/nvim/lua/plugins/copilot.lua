14806cdc71edb73fdca2388ff70bd0abf81fea45
roy.crippen4 <roy.crippen4@archarithms.com>
2024-11-21 13:58:38 -0500

    chore: update to `lts/jod`

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -8,7 +8,7 @@ return {
   opts = {
     panel = { enabled = false },
     suggestion = { enabled = true, auto_trigger = true, keymap = { accept = '<M-cr>' } },
-    copilot_node_command = vim.fn.expand('$HOME') .. '/.nvm/versions/node/v21.7.3/bin/node',
+    copilot_node_command = vim.fn.expand('$HOME') .. '/.nvm/versions/node/v22.11.0/bin/node',
     filetypes = {
       ocaml = false,
       rust = false,
