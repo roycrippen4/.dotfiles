b5c97490d5a27baaa268ce9ab9a6211844994d25
roy.crippen4 <roy.crippen4@archarithms.com>
2024-11-21 13:58:51 -0500

    chore: formatting

--- nvim/.config/nvim/lua/plugins/zone.lua
+++ nvim/.config/nvim/lua/plugins/zone.lua
@@ -1,12 +1,10 @@
----@type LazyPluginSpec[]
+---@type LazyPluginSpec
 return {
-  {
-    'nvzone/minty',
-    dependencies = { 'nvzone/volt' },
-    cmd = { 'Shades', 'Huefy' },
-    keys = {
-      { '<leader>cs', '<cmd>Shades<cr>' },
-      { '<leader>cp', '<cmd>Huefy<cr>' },
-    },
+  'nvzone/minty',
+  dependencies = { 'nvzone/volt' },
+  cmd = { 'Shades', 'Huefy' },
+  keys = {
+    { '<leader>cs', '<cmd>Shades<cr>' },
+    { '<leader>cp', '<cmd>Huefy<cr>' },
   },
 }
