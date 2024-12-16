66f1af82d414ff6715de8ffc00aa938a8af3039f
roy.crippen4 <roy.crippen4@archarithms.com>
2024-12-15 20:19:34 -0500

    rollback last two commits...

--- nvim/.config/nvim/after/ftplugin/lua.lua
+++ nvim/.config/nvim/after/ftplugin/lua.lua
@@ -11,22 +11,11 @@ require('nvim-surround').buffer_setup({
   },
 })
 
----@return boolean?
-local function should_format_lua_func()
-  local col = vim.fn.getpos('.')[3]
-  local start_pos, end_pos = vim.api.nvim_get_current_line():find('%)%s*end')
-  return start_pos and col > start_pos and col <= end_pos
-end
-
-vim.keymap.set('i', '<cr>', function()
-  if should_format_lua_func() then
-    feed('<cr><esc>O', 'n')
-  else
-    return '<cr>'
-  end
-end, { expr = true })
-
 vim.keymap.set('i', '<', function()
+  if vim.bo.ft ~= 'lua' then
+    return '<'
+  end
+
   local pos = vim.api.nvim_win_get_cursor(0)
   local str = vim.treesitter.get_node({ pos = { pos[1] - 1, pos[2] } }):type()
 
