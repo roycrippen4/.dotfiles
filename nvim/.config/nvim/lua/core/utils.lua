d1d98446efc0842057bc9b4b81316daf6ae68dcc
roy.crippen4 <roy.crippen4@archarithms.com>
2024-12-21 14:27:41 -0500

    chore(nvim): formatting

--- nvim/.config/nvim/lua/core/utils.lua
+++ nvim/.config/nvim/lua/core/utils.lua
@@ -1,9 +1,3 @@
-local api = vim.api
-local fn = vim.fn
-local augroup = api.nvim_create_augroup
-local autocmd = api.nvim_create_autocmd
-local clear_autocmds = api.nvim_clear_autocmds
-
 _G.U = {}
 
 ---Generates an array of numbers from start to stop with step n
@@ -66,14 +60,10 @@ function U.range_iter(start, stop, step)
 end
 
 local function organize_imports()
-  -- local valid_fts = { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact' }
-
-  -- if vim.tbl_contains(valid_fts, vim.bo.filetype) then
   vim.lsp.buf.code_action({
     apply = true,
     context = { only = { 'source.organizeImports' }, diagnostics = {} },
   })
-  -- else
 end
 
 --- Toggles diagnostics
@@ -84,13 +74,13 @@ end
 local function toggle_inlay_hints()
   vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = 0 }))
   local msg = vim.lsp.inlay_hint.is_enabled({ bufnr = 0 }) and 'Lsp inlay hints enabled' or 'Lsp inlay hints disabled'
-  print(msg)
+  log(msg)
 end
 
 ---@param chars string[]
 local function check_trigger_chars(chars)
-  local cur_line = api.nvim_get_current_line()
-  local pos = api.nvim_win_get_cursor(0)[2]
+  local cur_line = vim.api.nvim_get_current_line()
+  local pos = vim.api.nvim_win_get_cursor(0)[2]
   return vim.iter(chars):any(function(char)
     return cur_line:sub(pos, pos) == char or cur_line:sub(pos - 1, pos) == ', '
   end)
@@ -100,10 +90,10 @@ end
 ---@param client vim.lsp.Client
 local function setup_signature_helper(bufnr, client)
   if client.server_capabilities.signatureHelpProvider then
-    local group = augroup('LspSignature', { clear = false })
-    clear_autocmds({ group = group, buffer = bufnr })
+    local group = vim.api.nvim_create_augroup('LspSignature', { clear = false })
+    vim.api.nvim_clear_autocmds({ group = group, buffer = bufnr })
 
-    autocmd({ 'TextChangedI', 'TextChangedP', 'InsertEnter' }, {
+    vim.api.nvim_create_autocmd({ 'TextChangedI', 'TextChangedP', 'InsertEnter' }, {
       group = group,
       buffer = bufnr,
       callback = function()
@@ -118,8 +108,8 @@ end
 ---@param client vim.lsp.Client
 local function svelte(client)
   if client.name == 'svelte' then
-    autocmd('BufWritePost', {
-      group = augroup('svelte_ondidchangetsorjsfile', { clear = true }),
+    vim.api.nvim_create_autocmd('BufWritePost', {
+      group = vim.api.nvim_create_augroup('svelte_ondidchangetsorjsfile', { clear = true }),
       pattern = { '*.js', '*.ts' },
       callback = function(ctx)
         client.notify('$/onDidChangeTsOrJsFile', { uri = ctx.match })
@@ -254,28 +244,20 @@ U.capabilities.textDocument.completion.completionItem = {
 ---@param key string
 ---@param mode string
 function _G.feed(key, mode)
-  api.nvim_feedkeys(api.nvim_replace_termcodes(key, true, true, true), mode, true)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
 end
 
 ---@param diagnostics vim.Diagnostic[]
 function U.add_missing_commas(diagnostics)
   vim.iter(diagnostics):each(function(diag)
     if diag.message == 'Miss symbol `,` or `;` .' or diag.message == 'Missed symbol `,`.' then
-      api.nvim_buf_set_text(0, diag.lnum, diag.col, diag.lnum, diag.col, { ',' })
+      vim.api.nvim_buf_set_text(0, diag.lnum, diag.col, diag.lnum, diag.col, { ',' })
     end
   end)
 end
 
-function U.close_buf()
-  -- if #api.nvim_list_wins() == 1 and string.sub(api.nvim_buf_get_name(0), -10) == 'NvimTree_1' then
-  --   vim.cmd([[ q ]])
-  -- else
-  require('local.tabufline').close_buffer()
-  -- end
-end
-
 function U.send_to_black_hole()
-  local line_content = fn.line('.')
+  local line_content = vim.fn.line('.')
   if type(line_content) == 'string' and string.match(line_content, '^%s*$') then
     vim.cmd('normal! "_dd')
   else
@@ -344,7 +326,7 @@ end
 
 ---@param filename string
 function U.has_file(filename)
-  return fn.filereadable(fn.getcwd() .. '/' .. filename) == 1 and true or false
+  return vim.fn.filereadable(vim.fn.getcwd() .. '/' .. filename) == 1 and true or false
 end
 
 ---@type fun(ctx: {buf: integer, event?: string, file?: string, id?: integer, match?: string}): nil
