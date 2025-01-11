e00d9efccef4a299cbcccb2981e1cc1da9a7d25e
roy.crippen4 <roy.crippen4@archarithms.com>
2025-01-11 15:41:29 -0500

    feat(nvim): zen mode

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -4,11 +4,28 @@ return {
   'folke/snacks.nvim',
   priority = 1000,
   lazy = false,
+  keys = { {
+    '<leader>z',
+    function()
+      Snacks.zen()
+    end,
+  } },
   ---@type snacks.Config
   opts = {
+    styles = {
+      notification = { wo = { wrap = true } },
+      zen = {
+        max_height = 63,
+        width = 160,
+        backdrop = { blend = 20 },
+      },
+    },
     notifier = { timeout = 5000, enabled = true },
     bigfile = { enabled = true },
-    styles = { notification = { wo = { wrap = true } } },
+    zen = {
+      toggles = { dim = false },
+      show = { statusline = true, tabline = true },
+    },
   },
   init = function()
     vim.api.nvim_create_autocmd('VimEnter', {
