7c0007d778cdfa2ff6b488f4a8e8527e7fdfa3de
roy.crippen4 <roy.crippen4@archarithms.com>
2025-01-16 20:23:05 -0500

    chore(nvim): considering removing rainbow-delims

--- nvim/.config/nvim/lua/plugins/rainbow_delimiters.lua
+++ nvim/.config/nvim/lua/plugins/rainbow_delimiters.lua
@@ -1,24 +1,24 @@
 ---@type LazyPluginSpec
 return {
-  'hiphish/rainbow-delimiters.nvim', --- https://github.com/HiPhish/rainbow-delimiters.nvim
-  event = 'VimEnter',
-  config = function()
-    require('rainbow-delimiters.setup').setup({
-      strategy = {
-        [''] = require('rainbow-delimiters').strategy['global'],
-        vim = require('rainbow-delimiters').strategy['local'],
-      },
-      query = { [''] = 'rainbow-delimiters' },
-      priority = { [''] = 110 },
-      highlight = {
-        'RainbowDelimiterRed',
-        'RainbowDelimiterYellow',
-        'RainbowDelimiterBlue',
-        'RainbowDelimiterOrange',
-        'RainbowDelimiterGreen',
-        'RainbowDelimiterViolet',
-        'RainbowDelimiterCyan',
-      },
-    })
-  end,
+  -- 'hiphish/rainbow-delimiters.nvim', --- https://github.com/HiPhish/rainbow-delimiters.nvim
+  -- event = 'VimEnter',
+  -- config = function()
+  --   require('rainbow-delimiters.setup').setup({
+  --     strategy = {
+  --       [''] = require('rainbow-delimiters').strategy['global'],
+  --       vim = require('rainbow-delimiters').strategy['local'],
+  --     },
+  --     query = { [''] = 'rainbow-delimiters' },
+  --     priority = { [''] = 110 },
+  --     highlight = {
+  --       'RainbowDelimiterRed',
+  --       'RainbowDelimiterYellow',
+  --       'RainbowDelimiterBlue',
+  --       'RainbowDelimiterOrange',
+  --       'RainbowDelimiterGreen',
+  --       'RainbowDelimiterViolet',
+  --       'RainbowDelimiterCyan',
+  --     },
+  --   })
+  -- end,
 }
