4736daa54e695a31354fee4683877c7097e6d324
roy.crippen4 <roy.crippen4@archarithms.com>
2025-01-25 15:00:52 -0500

    nvim: disabled scope for `ibl`

--- nvim/.config/nvim/lua/plugins/blankline.lua
+++ nvim/.config/nvim/lua/plugins/blankline.lua
@@ -8,15 +8,6 @@ return {
   ---@type ibl.config
   opts = {
     indent = { char = '▏' },
-    scope = {
-      char = '▏',
-      include = {
-        node_type = {
-          lua = { 'return_statement', 'table_constructor' },
-          jsx = { 'jsx_self_closing_element' },
-          tsx = { 'jsx_self_closing_element' },
-        },
-      },
-    },
+    scope = { enabled = false },
   },
 }
