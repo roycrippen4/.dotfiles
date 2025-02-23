691a8f683b2c73fb1bfb0b6f675d3199a52df10c
roy.crippen4 <roy.crippen4@archarithms.com>
2025-02-23 00:07:05 -0500

    feat(nvim): added ocaml `run project` keymap

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -11,6 +11,9 @@ vim.api.nvim_create_autocmd('VimEnter', {
   end,
 })
 
+---@type string?
+local ocaml_proj = nil
+
 local function test_current_file()
   local ft = vim.bo.ft
   local file = vim.fn.expand('%')
@@ -44,6 +47,24 @@ local function run_current_file()
 
   if ft == 'typescript' or ft == 'javascript' then
     vim.cmd('TermExec direction=horizontal size=16 cmd="bun run ' .. file .. '"')
+    return
+  end
+
+  if ft == 'ocaml' then
+    if not ocaml_proj then
+      local cwd = vim.fn.getcwd()
+      local cwd_content = vim.split(vim.fn.glob(cwd .. '/*'), '\n', { trimempty = true })
+
+      ---@type string
+      local project_abs_filepath = vim.iter(cwd_content):find(function(path)
+        if path:match('%.opam') then
+          return true
+        end
+      end)
+      ocaml_proj = vim.split(vim.fn.fnamemodify(project_abs_filepath, ':t'), '.opam')[1]
+    end
+
+    vim.cmd("TermExec direction=horizontal size=16 cmd='dune build && dune exec " .. ocaml_proj .. "'")
     return
   end
 
