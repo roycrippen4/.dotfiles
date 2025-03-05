dcebeade0eab707c8a791bd2015d0e8c8a1be3fb
roy.crippen4 <roy.crippen4@archarithms.com>
2025-03-05 13:42:58 -0500

    fix(nvim): use OSC 52 copy/paste during ssh

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,6 +1,20 @@
 local opt = vim.opt
 local g = vim.g
 
+if os.getenv('SSH_TTY') ~= nil or os.getenv('SSH_CONNECTION') ~= nil then
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+  }
+end
+
 g.loaded_python3_provider = 0
 g.loaded_ruby_provider = 0
 g.loaded_perl_provider = 0
@@ -16,28 +30,27 @@ g.loaded_netrwPlugin = 1
 -- General
 -----------------------------------------------------------
 -- stylua: ignore start
--- opt.completeopt   = { 'menu', 'menuone', 'noselect' } -- Sets the completion options
-opt.scrolloff     = 14                                -- Show x lines above and below the cursor
-opt.signcolumn    = 'yes'                             -- Show signs in the signcolumn
-opt.ignorecase    = true                              -- Ignores case in search
-opt.smartcase     = true                              -- Override the 'ignorecase' option if the search pattern contains upper case characters
-opt.splitbelow    = true                              -- Split below instead of above
-opt.splitright    = true                              -- Split right instead of left
-opt.termguicolors = true                              -- Enable true color support
-opt.undofile      = true                              -- Save undo history to file
-opt.updatetime    = 250                               -- interval for writing swap file to disk, also used by gitsigns
-opt.cmdheight     = 1                                 -- Height of the command bar
-opt.formatoptions = 'j'                               -- Don't use vim for formatting
-opt.laststatus    = 3                                 -- global statusline
-opt.showmode      = false                             -- Don't display mode
-opt.title         = true                              -- Show the title in the window titlebar
-opt.wrap          = false                             -- Display long lines as just one line
-opt.whichwrap:append('<>[]hl')                        -- go to previous/next line with h,l,left arrow and right arrow
-opt.clipboard = 'unnamedplus'                         -- Use system clipboard
-opt.cursorline = true                                 -- Enable highlighting of the current line
-opt.hlsearch = true
-opt.swapfile = false
-opt.smoothscroll = true
+opt.scrolloff     = 14         -- Show x lines above and below the cursor
+opt.signcolumn    = 'yes'      -- Show signs in the signcolumn
+opt.ignorecase    = true       -- Ignores case in search
+opt.smartcase     = true       -- Override the 'ignorecase' option if the search pattern contains upper case characters
+opt.splitbelow    = true       -- Split below instead of above
+opt.splitright    = true       -- Split right instead of left
+opt.termguicolors = true       -- Enable true color support
+opt.undofile      = true       -- Save undo history to file
+opt.updatetime    = 250        -- interval for writing swap file to disk, also used by gitsigns
+opt.cmdheight     = 1          -- Height of the command bar
+opt.formatoptions = 'j'        -- Don't use vim for formatting
+opt.laststatus    = 3          -- global statusline
+opt.showmode      = false      -- Don't display mode
+opt.title         = true       -- Show the title in the window titlebar
+opt.wrap          = false      -- Display long lines as just one line
+opt.whichwrap:append('<>[]hl') -- go to previous/next line with h,l,left arrow and right arrow
+opt.clipboard = 'unnamedplus'  -- Use system clipboard
+opt.cursorline = true          -- Enable highlighting of the current line
+opt.hlsearch = true            -- Enable highlight search
+opt.swapfile = false           -- Disable swap file
+opt.smoothscroll = true        -- Enable smooth scrolling
 
 -----------------------------------------------------------
 -- Indent settings
