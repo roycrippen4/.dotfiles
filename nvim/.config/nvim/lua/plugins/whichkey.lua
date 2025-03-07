45dadcb4fd025053551ab27558f6ec5269efef4a
roy.crippen4 <roy.crippen4@archarithms.com>
2025-03-07 15:03:59 -0500

    chore(nvim): update which-key to match new keymaps

--- nvim/.config/nvim/lua/plugins/whichkey.lua
+++ nvim/.config/nvim/lua/plugins/whichkey.lua
@@ -1,21 +1,20 @@
 ---@type LazyPluginSpec
 return {
   'folke/which-key.nvim', -- https://github.com/folke/which-key.nvim
-  opts = {},
-  config = function()
-    require('which-key').add({
+  ---@type wk.Opts
+  opts = {
+    spec = {
       --stylua: ignore start
-      { '<leader>c',  group = '[C]rates',     icon = '' },
       { '<leader>d',  group = '[D]ebug',      icon = '' },
       { '<leader>f',  group = '[F]ind',       icon = '' },
-      { '<leader>fg', group = '[F]ind [G]it', icon = '' },
       { '<leader>g',  group = '[G]it',        icon = '' },
       { '<leader>i',  group = '[I]nspect',    icon = '󱡴' },
       { '<leader>l',  group = '[L]SP',        icon = '' },
-      { '<leader>p',  group = '[P]ackage',    icon = '' },
+      { '<leader>n',  group = '[N]eotest',    icon = '󰙨' },
       { '<leader>s',  group = '[S]cissors',   icon = '' },
       { '<leader>t',  group = '[T]rouble',    icon = '' },
-      --stylua: ignore end
-    })
-  end,
+      { '<leader>c',  group = '[C]rates',     icon = '', mode = { "v", "n" } },
+      -- stylua: ignore end
+    },
+  },
 }
