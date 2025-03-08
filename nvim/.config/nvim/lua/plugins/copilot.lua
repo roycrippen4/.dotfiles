46c381d4cf3b342c8b63289af572615f9f4e0233
roy.crippen4 <roy.crippen4@archarithms.com>
2025-03-07 22:46:36 -0500

    chore(nvim): I might ditch copilot...

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -2,6 +2,7 @@
 ---@type LazyPluginSpec
 return {
   'zbirenbaum/copilot.lua', -- https://github.com/zbirenbaum/copilot.lua
+  enable = false,
   event = 'BufReadPost',
   keys = { { '<leader>tc', '<cmd> Copilot toggle <cr>' } },
   ---@type copilot_config
@@ -9,10 +10,6 @@ return {
     panel = { enabled = false },
     suggestion = { enabled = true, auto_trigger = true, keymap = { accept = '<M-cr>' } },
     copilot_node_command = vim.fn.expand('$HOME') .. '/.nvm/versions/node/v22.11.0/bin/node',
-    filetypes = {
-      ocaml = false,
-      rust = false,
-      zig = false,
-    },
+    filetypes = { ocaml = false, rust = false, zig = false },
   },
 }
