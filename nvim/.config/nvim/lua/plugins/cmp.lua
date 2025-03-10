e6183fcfad857576a60a6ed3b1619a5a92a7cad0
roy.crippen4 <roy.crippen4@archarithms.com>
2025-03-10 13:15:58 -0400

    fix(nvim): fix `blink.cmp` update delay warning

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -66,7 +66,7 @@ return {
       documentation = {
         auto_show = true,
         auto_show_delay_ms = 250,
-        update_delay_ms = 25,
+        update_delay_ms = 50,
         window = { min_width = 20, max_width = 80, max_height = 20, border = 'rounded' },
       },
     },
