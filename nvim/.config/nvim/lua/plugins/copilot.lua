e91105d2180229a2c9b62a90a6d126643d56f631
roy.crippen4 <roy.crippen4@archarithms.com>
2025-03-16 19:04:57 -0400

    chore(nvim): disable copilot

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,15 +1,15 @@
 ---@module "copilot"
 ---@type LazyPluginSpec
 return {
-  'zbirenbaum/copilot.lua', -- https://github.com/zbirenbaum/copilot.lua
-  enable = false,
-  event = 'BufReadPost',
-  keys = { { '<leader>tc', '<cmd> Copilot toggle <cr>' } },
-  ---@type copilot_config
-  opts = {
-    panel = { enabled = false },
-    suggestion = { enabled = true, auto_trigger = true, keymap = { accept = '<M-cr>' } },
-    copilot_node_command = vim.fn.expand('$HOME') .. '/.nvm/versions/node/v22.11.0/bin/node',
-    filetypes = { ocaml = false, rust = false, zig = false },
-  },
+  -- 'zbirenbaum/copilot.lua', -- https://github.com/zbirenbaum/copilot.lua
+  -- enable = false,
+  -- event = 'BufReadPost',
+  -- keys = { { '<leader>tc', '<cmd> Copilot toggle <cr>' } },
+  -- ---@type copilot_config
+  -- opts = {
+  --   panel = { enabled = false },
+  --   suggestion = { enabled = true, auto_trigger = true, keymap = { accept = '<M-cr>' } },
+  --   copilot_node_command = vim.fn.expand('$HOME') .. '/.nvm/versions/node/v22.11.0/bin/node',
+  --   filetypes = { ocaml = false, rust = false, zig = false },
+  -- },
 }
