dc5c1a263e0f424f94ab33d1cec15c1575bedd67
roy.crippen4 <roy.crippen4@archarithms.com>
2025-03-17 18:42:46 -0400

    feat(nvim): in-progress menu stuff

--- nvim/.config/nvim/lua/utils.lua
+++ nvim/.config/nvim/lua/utils.lua
@@ -116,4 +116,71 @@ function M.find_node_ancestor(types, node)
   return M.find_node_ancestor(types, node:parent())
 end
 
+function M.tbl_length(T)
+  local count = 0
+  for _ in pairs(T) do
+    count = count + 1
+  end
+  return count
+end
+
+---Returns the list of currently selected lines
+---@return string?
+function M.get_visual_selection()
+  -- this will exit visual mode
+  -- use 'gv' to reselect the text
+  local _, csrow, cscol, cerow, cecol
+  local mode = vim.fn.mode()
+  local valid_modes = { 'v', 'V', '' }
+  if not vim.tbl_contains(valid_modes, mode) then
+    vim.notify('exiting', vim.log.levels.INFO)
+    return
+  end
+
+  -- if we are in visual mode use the live position
+  _, csrow, cscol, _ = unpack(vim.fn.getpos('.'))
+  _, cerow, cecol, _ = unpack(vim.fn.getpos('v'))
+  if mode == 'V' then
+    -- visual line doesn't provide columns
+    cscol, cecol = 0, 999
+  end
+  -- exit visual mode
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'n', true)
+
+  -- swap vars if needed
+  if cerow < csrow then
+    csrow, cerow = cerow, csrow
+  end
+  if cecol < cscol then
+    cscol, cecol = cecol, cscol
+  end
+  local lines = vim.fn.getline(csrow, cerow)
+  if type(lines) == 'string' then
+    lines = { lines }
+  end
+
+  local n = M.tbl_length(lines)
+
+  if n <= 0 then
+    return ''
+  end
+
+  lines[n] = string.sub(lines[n], 1, cecol)
+  lines[1] = string.sub(lines[1], cscol)
+  return table.concat(lines, '\n')
+end
+
+--- TODO: Try to add the hl_yank stuff
+function M.visual_yank()
+  local selection = M.get_visual_selection()
+  if not selection then
+    return
+  end
+
+  vim.fn.setreg('+', selection)
+  vim.hl.on_yank()
+end
+
+vim.keymap.set('v', '<M-I>', M.visual_yank)
+
 return M
