d8f5bd913a7945677b16a3c55798af89f7cd6d4e
roy.crippen4 <roy.crippen4@archarithms.com>
2025-03-22 18:26:35 -0400

    chore(nvim): use "rounded" for `nvim-scissors` config

--- nvim/.config/nvim/lua/plugins/scissors.lua
+++ nvim/.config/nvim/lua/plugins/scissors.lua
@@ -2,7 +2,11 @@
 ---@type LazyPluginSpec
 return {
   'chrisgrieser/nvim-scissors', -- https://github.com/chrisgrieser/nvim-scissors
-  opts = { snippetDir = vim.fn.stdpath('config') .. '/snippets' },
+  ---@type Scissors.Config
+  opts = {
+    snippetDir = vim.fn.stdpath('config') .. '/snippets',
+    editSnippetPopup = { border = 'rounded' },
+  },
   keys = {
     { mode = { 'n', 'x' }, '<leader>sa', '<cmd> ScissorsAddNewSnippet <cr>', desc = '[A]dd snippet' },
     { mode = 'n', '<leader>se', '<cmd> ScissorsEditSnippet <cr>', desc = '[E]dit snippet' },
