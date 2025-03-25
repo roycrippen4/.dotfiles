4c806b2f332952faf4a08cdf11e75035c1ea5af4
roy.crippen4 <roy.crippen4@archarithms.com>
2025-03-24 21:54:51 -0400

    feat(nvim): better zig keybinds

--- nvim/.config/nvim/after/ftplugin/zig.lua
+++ nvim/.config/nvim/after/ftplugin/zig.lua
@@ -27,7 +27,7 @@ local function conditional_hover()
   end
 
   local test_name = vim.treesitter.get_node_text(str_content, 0)
-  local test_cmd = ('zig test %s --test-filter "%s"'):format(vim.fn.expand('%'), test_name)
+  local test_cmd = ('zig build test -Dtest-filter="%s"'):format(test_name)
   local cmd = ("direction=horizontal size=16 cmd='%s'"):format(test_cmd)
   vim.g.zig_test_cmd = cmd
   vim.cmd.TermExec(cmd)
@@ -43,10 +43,16 @@ end
 
 local function run_last_test()
   if not vim.g.zig_test_cmd then
-    vim.notify('No previous test found')
+    vim.g.zig_test_cmd = 'direction=horizontal size=16 cmd="zig build test"'
+    vim.cmd.TermExec(vim.g.zig_test_cmd)
     return
   end
 
+  vim.cmd.TermExec(vim.g.zig_test_cmd)
+end
+
+local function run_build_test()
+  vim.g.zig_test_cmd = 'direction=horizontal size=16 cmd="zig build test"'
   vim.cmd.TermExec(vim.g.zig_test_cmd)
 end
 
@@ -56,6 +62,7 @@ end
 
 vim.keymap.set('n', '<leader>b', build, { desc = '[B]uild project', buffer = true })
 vim.keymap.set('n', '<leader>lr', run_file, { desc = 'Run current file', buffer = true })
+vim.keymap.set('n', '<leader>lT', run_build_test, { desc = 'Run build test', buffer = true })
 vim.keymap.set('n', '<leader>lt', run_last_test, { desc = 'Run last test', buffer = true })
 vim.keymap.set('n', 'K', conditional_hover, { desc = "Conditionally runs test if it's under the cursor", buffer = true })
 vim.keymap.set('n', 'R', run_project, { desc = 'Run current project', buffer = true })
