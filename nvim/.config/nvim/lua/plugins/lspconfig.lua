00d3497949498d4be33fb597378a75487bc9daa4
roy.crippen4 <roy.crippen4@archarithms.com>
2025-04-08 19:57:03 -0400

    chore: small config changes

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -110,12 +110,8 @@ return {
     })
 
     configure_server('zls', {
-      root_dir = lspconfig.util.root_pattern('.git', 'build.zig', 'zls.json'),
       settings = {
-        zls = {
-          enable_build_on_save = true,
-          enable_snippets = false,
-        },
+        zls = { enable_snippets = false },
       },
     })
 
