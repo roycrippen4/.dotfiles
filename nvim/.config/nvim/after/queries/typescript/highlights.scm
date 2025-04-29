da1fe8bc9dd5ec3ebe772205a38ca9ce3311a502
roy.crippen4 <roy.crippen4@archarithms.com>
2025-04-29 17:07:10 -0400

    fix(nvim): simplify typescript treesitter queries

--- nvim/.config/nvim/after/queries/typescript/highlights.scm
+++ nvim/.config/nvim/after/queries/typescript/highlights.scm
@@ -1,22 +1,14 @@
 ;; extends
 
-(type_arguments
-  (tuple_type
-    (type_identifier) @type.type_argument))
+((nested_type_identifier) @type.type_argument
+  (#has-ancestor? @type.type_argument type_arguments))
 
-(type_arguments
-  (tuple_type
-    (predefined_type) @type.type_argument))
+((predefined_type) @type.type_argument
+  (#has-ancestor? @type.type_argument type_arguments))
 
-(type_arguments
-  (predefined_type) @type.type_argument)
+((type_identifier) @type.type_argument
+  (#has-ancestor? @type.type_argument type_arguments))
 
-(type_arguments
-  (type_identifier) @type.type_argument)
+((literal_type) @type.type_argument
+ (#has-ancestor? @type.type_argument type_arguments))
 
-(type_arguments
-  (union_type (literal_type _) @type.type_argument))
-
-(type_arguments
-  (union_type
-    (type_identifier) @type.type_argument))
