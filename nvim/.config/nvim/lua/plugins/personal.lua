73db71fb5f8091609488569003779482bdf1fb72
roy.crippen4 <roy.crippen4@archarithms.com>
2025-05-07 16:22:06 -0400

    chore: update personal neovim plugins

--- nvim/.config/nvim/lua/plugins/personal.lua
+++ nvim/.config/nvim/lua/plugins/personal.lua
@@ -3,22 +3,21 @@ return {
   {
     --- Harpoon clone
     'roycrippen4/poon.nvim', -- https://github.com/roycrippen4/poon.nvim
-    lazy = true,
+    cmd = { 'PoonJump', 'PoonRemoveMark', 'PoonAddMark', 'PoonToggle' },
     keys = {
-      -- stylua: ignore start
-      { '<c-f>',   function() require('poon').mark.set()       end },
-      { 'B',       function() require('poon').mark.set_first() end },
-      { '<c-e>',   function() require('poon').menu.toggle()    end },
-      { '<c-1>',   function() require('poon').mark.jump(1)     end },
-      { '<c-2>',   function() require('poon').mark.jump(2)     end },
-      { '<c-3>',   function() require('poon').mark.jump(3)     end },
-      { '<c-4>',   function() require('poon').mark.jump(4)     end },
-      { '<c-5>',   function() require('poon').mark.jump(5)     end },
-      { '<c-6>',   function() require('poon').mark.jump(6)     end },
-      { '<c-7>',   function() require('poon').mark.jump(7)     end },
-      { '<c-8>',   function() require('poon').mark.jump(8)     end },
-      { '<c-9>',   function() require('poon').mark.jump(9)     end },
-      -- stylua: ignore end
+      { '<c-f>', '<cmd> PoonAdd <cr>' },
+      { 'B', '<cmd> PoonAdd! <cr>' },
+      { '<c-e>', '<cmd> PoonToggle <cr>' },
+      { '<c-x>', '<cmd> PoonRemove <cr>' },
+      { '<c-1>', '<cmd> PoonJump 1 <cr>' },
+      { '<c-2>', '<cmd> PoonJump 2 <cr>' },
+      { '<c-3>', '<cmd> PoonJump 3 <cr>' },
+      { '<c-4>', '<cmd> PoonJump 4 <cr>' },
+      { '<c-5>', '<cmd> PoonJump 5 <cr>' },
+      { '<c-6>', '<cmd> PoonJump 6 <cr>' },
+      { '<c-7>', '<cmd> PoonJump 7 <cr>' },
+      { '<c-8>', '<cmd> PoonJump 8 <cr>' },
+      { '<c-9>', '<cmd> PoonJump 9 <cr>' },
     },
     opts = {},
   },
@@ -27,20 +26,27 @@ return {
     'roycrippen4/beeline.nvim', -- https://github.com/roycrippen4/beeline.nvim
     lazy = false,
     keys = {
-      { '<leader>x', '<cmd>BeelineBufClose<cr>', { desc = 'Close buffer' } },
-      { 'H', '<cmd>BeelineBufPrev<cr>', { desc = 'Go to previous beeline buffer' } },
-      { 'L', '<cmd>BeelineBufNext<cr>', { desc = 'Go to next beeline buffer' } },
+      { '<leader>x', '<cmd> BeelineBufClose <cr>', { desc = 'Close buffer' } },
+      { 'H', '<cmd> BeelineBufPrev <cr>', { desc = 'Go to previous beeline buffer' } },
+      { 'L', '<cmd> BeelineBufNext <cr>', { desc = 'Go to next beeline buffer' } },
     },
     opts = {},
   },
   {
     --- Highlight group inspection tools
     'roycrippen4/inspector.nvim', -- https://github.com/roycrippen4/inspector.nvim
-    -- stylua: ignore
+    cmd = { 'InspectFloat', 'InspectSplit' },
     keys = {
-      { '<leader>if', function() require('inspector').inspect_in_float() end, desc = "[I]nspect in float" },
-      { '<leader>iw', function() require('inspector').inspect_in_split() end, desc = "[I]nspect in split" },
+      { '<leader>if', '<cmd> InspectFloat <cr>', desc = '[I]nspect in float' },
+      { '<leader>iw', '<cmd> InspectSplit <cr>', desc = '[I]nspect in split' },
     },
+    opts = {},
+  },
+  {
+    --- Fork of PackageInfo.nvim
+    'roycrippen4/package-info.nvim', -- https://github.com/roycrippen4/package-info.nvim
+    event = { 'BufRead package.json' },
+    keys = { { '<leader>nr', '<cmd> PackageInfoRunScript <cr>', desc = 'Run script under the cursor' } },
     opts = {},
   },
 }
