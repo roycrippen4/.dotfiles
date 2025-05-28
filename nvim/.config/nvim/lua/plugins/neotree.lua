8e0cd3acfb49f6561a597c844d355ca30f66aa88
roy.crippen4 <roy.crippen4@archarithms.com>
2025-05-28 12:40:55 -0400

    feat(nvim): copy paths from neotree

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -1,3 +1,48 @@
+local function copy_path(state)
+  local node = state.tree:get_node()
+  local filepath = node:get_id()
+  local filename = node.name
+  local modify = vim.fn.fnamemodify
+
+  local results = {
+    filepath,
+    modify(filepath, ':.'),
+    modify(filepath, ':~'),
+    filename,
+    modify(filename, ':r'),
+    modify(filename, ':e'),
+  }
+
+  vim.ui.select({
+    '1. Absolute path: ' .. results[1],
+    '2. Path relative to CWD: ' .. results[2],
+    '3. Path relative to HOME: ' .. results[3],
+    '4. Filename: ' .. results[4],
+    '5. Filename without extension: ' .. results[5],
+    '6. Extension of the filename: ' .. results[6],
+  }, { prompt = 'Choose to copy to clipboard:' }, function(choice)
+    if choice then
+      local i = tonumber(choice:sub(1, 1))
+      if i then
+        local result = results[i]
+        vim.fn.setreg('+', result)
+        vim.notify('Copied: ' .. result)
+      else
+        vim.notify('Invalid selection')
+      end
+    else
+      vim.notify('Selection cancelled')
+    end
+  end)
+end
+
+local function copy_absolute_path(state)
+  local node = state.tree:get_node()
+  local filepath = node:get_id()
+  vim.fn.setreg('+', filepath)
+  vim.notify('Copied absolute path to clipboard')
+end
+
 return {
   'nvim-neo-tree/neo-tree.nvim',
   branch = 'v3.x',
@@ -11,6 +56,12 @@ return {
     filesystem = {
       follow_current_file = { enabled = true },
       window = { mappings = { ['/'] = 'noop' } },
+    },
+    window = {
+      mappings = {
+        ['Y'] = copy_path,
+        ['gy'] = copy_absolute_path,
+      },
     },
   },
 }
