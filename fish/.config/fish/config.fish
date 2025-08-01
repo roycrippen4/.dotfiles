6ce2eee4c148c40c75b99b02faa464c260344e3f
roy.crippen4 <roy.crippen4@archarithms.com>
2025-08-01 02:23:59 -0400

    chore: add .cargo to PATH
    how tf has it been working this whole time?

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,9 +8,10 @@ end
 
 if status is-interactive
     fish_add_path $HOME/.bun/bin
-    fish_add_path $HOME/.local/share/bob/nvim-bin/
+    fish_add_path $HOME/.local/share/bob/nvim-bin
     fish_add_path $HOME/.local/bin
-    fish_add_path /usr/local/man/
+    fish_add_path /usr/local/man
+    fish_add_path $HOME/.cargo/bin
 
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
