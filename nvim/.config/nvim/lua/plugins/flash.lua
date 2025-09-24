2522b3a283cb1608ce0d80fd6f83def60ddc1e84
roy.crippen4 <roy.crippen4@archarithms.com>
2025-09-24 15:32:59 -0400

    chore(nvim): formatting

--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -9,12 +9,39 @@ return {
       win_config = { border = 'none' },
     },
   },
-  -- stylua: ignore
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", false, mode = { 'v' } },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    {
+      's',
+      mode = { 'n', 'x', 'o' },
+      function()
+        require('flash').jump()
+      end,
+      desc = 'Flash',
+    },
+    { 'S', false, mode = { 'v' } },
+    {
+      'r',
+      mode = 'o',
+      function()
+        require('flash').remote()
+      end,
+      desc = 'Remote Flash',
+    },
+    {
+      'R',
+      mode = { 'o', 'x' },
+      function()
+        require('flash').treesitter_search()
+      end,
+      desc = 'Treesitter Search',
+    },
+    {
+      '<c-s>',
+      mode = { 'c' },
+      function()
+        require('flash').toggle()
+      end,
+      desc = 'Toggle Flash Search',
+    },
   },
 }
