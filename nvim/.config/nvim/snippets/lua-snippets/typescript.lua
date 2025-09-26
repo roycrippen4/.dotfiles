31439631a4400895305318d56ef574b3a8f379c7
roy.crippen4 <roy.crippen4@archarithms.com>
2025-09-26 19:02:08 -0400

    chore(nvim): remove snippets never use

--- nvim/.config/nvim/snippets/lua-snippets/typescript.lua
+++
@@ -1,8 +0,0 @@
----@diagnostic disable: undefined-global
-return {
-  postfix('.promise', {
-    f(function(_, parent)
-      return 'Promise<' .. parent.snippet.env.POSTFIX_MATCH .. '>'
-    end, {}),
-  }),
-}
