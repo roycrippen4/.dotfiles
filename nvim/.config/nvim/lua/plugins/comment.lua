10396aecb9f153425308462ef18db123f88a0c46
roy.crippen4 <roy.crippen4@archarithms.com>
2025-09-26 18:50:16 -0400

    chore(nvim): trying out native comment system

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,17 +1,17 @@
 ---@type LazyPluginSpec
 return {
-  'numToStr/Comment.nvim', -- https://github.com/numToStr/Comment.nvim
-  event = 'BufEnter',
-  dependencies = {
-    {
-      'JoosepAlviste/nvim-ts-context-commentstring', -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring
-      opts = { enable_autocmd = false },
-    },
-  },
-  config = function()
-    require('Comment.ft').ocamlinterface = '(*%s*)'
-    require('Comment').setup({
-      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-    })
-  end,
+  -- 'numToStr/Comment.nvim', -- https://github.com/numToStr/Comment.nvim
+  -- event = 'BufEnter',
+  -- dependencies = {
+  --   {
+  --     'JoosepAlviste/nvim-ts-context-commentstring', -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring
+  --     opts = { enable_autocmd = false },
+  --   },
+  -- },
+  -- config = function()
+  --   require('Comment.ft').ocamlinterface = '(*%s*)'
+  --   require('Comment').setup({
+  --     pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+  --   })
+  -- end,
 }
