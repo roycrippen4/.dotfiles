eb6ec15f223a02c54c22faaf02010574bcdc7534
roy.crippen4 <roy.crippen4@archarithms.com>
2025-10-06 12:39:36 -0400

    feat(nvim): snippets

--- nvim/.config/nvim/after/ftplugin/rust.lua
+++ nvim/.config/nvim/after/ftplugin/rust.lua
@@ -1,5 +1,5 @@
 vim.keymap.set('n', '<leader>lr', '<cmd> RustRun <cr>', { desc = '[R]un file', buffer = true })
 vim.keymap.set('n', '<leader>lt', '<cmd> RustLsp! testables <cr>', { desc = '[T]est file', buffer = true })
 vim.keymap.set('n', 'K', '<cmd> RustLsp hover actions <cr>', { buffer = true })
-vim.keymap.set('n', '<leader>la', '<cmd> codeAction <cr>', { buffer = true })
+-- vim.keymap.set('n', '<leader>la', '<cmd> codeAction <cr>', { buffer = true })
 -- vim.keymap.set('n', '<leader>ld', '<cmd> RustLsp debuggables <cr>', { desc = '[D]ebug', buffer = true })
